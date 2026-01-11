0d202b9eedccd658e394918df4544ea737ed0d7d
m47eus2 <158043802+m47eus2@users.noreply.github.com>
2026-01-11 20:39:23 +0100

    Adding lab4_3.py

--- lab4/lab4_3.py
+++ lab4/lab4_3.py
@@ -14,34 +14,29 @@ C = np.array([[1, 0]])
 R = np.hstack((B, A@B))
 #print(np.linalg.matrix_rank(R))
 
-wc = -1
+wc = 1
 
-k1 = ((wc**2)*c*l-1) / Vin
-k2 = ((c*l)/Vin)*(2*wc - 1/c*r)
+# k1 = ((wc**2)*c*l-1) / Vin
+# k2 = ((c*l)/Vin)*(2*wc - 1/c*r)
+# K = np.array([k1, k2])
 
+k1 = (c*l*wc**2 - 1) / Vin
+k2 = (c*l/Vin) * (2*wc - 1/(c*r))
 K = np.array([k1, k2])
 
 vd = 5
 xd = np.array([vd, 0]).T
 
-def control(t):
-    return np.array([[1]]) #return as an array with single element
-
-
 def deg1(t, x):
-    #x = np.array([x]).T
-
     e = xd - x
-    ufb = K @ e
-    ud = vd / Vin
-
+    ufb = K @ e          # skalar
+    ud = vd / Vin       # skalar
     u = ufb + ud
-    
-    dx = A @ x + B.flatten() @ u
-    
-    return np.ndarray.tolist(dx.T[0])
 
-res = solve_ivp(deg1, [0,10], [2,0], rtol=1e-10) #arguments rtol and atol sets calculation tolerance
+    dx = A @ x + B[:, 0] * u
+    return dx
+
+res = solve_ivp(deg1, [0,10], [2,0], rtol=1e-10, atol=1e-10)
 
 y = C @ np.array(res.y)
 y = np.ndarray.tolist(y[0].T)
@@ -50,6 +45,6 @@ plt.figure()
 plt.grid()
 plt.title("Odpowiedź układu")
 plt.plot(res.t, res.y[0], label="x1")
-#plt.plot(res.t, res.y[1], label="x2")
+plt.plot(res.t, res.y[1], label="x2")
 plt.legend()
 plt.show()
