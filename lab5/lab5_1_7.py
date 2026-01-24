5ee1f742a846407094ecdcbeff9d7cc02d56cec7
m47eus2 <158043802+m47eus2@users.noreply.github.com>
2026-01-24 12:46:05 +0100

    Update lab5

---
+++ lab5/lab5_1_7.py
@@ -0,0 +1,74 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.integrate import solve_ivp
+
+r = 5
+l = 0.1
+c = 0.01
+Vin = 8
+
+A = np.array([[0,1], [-1/(c*l), -1/(c*r)]])
+B = np.array([[0], [Vin/(c*l)]])
+C = np.array([[1, 0]])
+
+def generator(t):
+    vd = 3 + 2*np.sin(t)
+    vdD = 2*np.cos(t)
+    vdDD = -2*np.sin(t)
+    return [vd, vdD, vdDD]
+
+def deg1(t, x):
+    [vd, vdD, vdDD] = generator(t)
+    xd = np.array([vd, vdD]).T
+
+    e = xd - x
+    ufb = K @ e
+    
+    ud = vd / Vin
+    u = ufb + ud
+
+    dx = A @ x + B[:, 0] * u
+    return dx
+
+def makeGraph(title, sim):
+    zadana = np.zeros_like(sim.t)
+    zadana = [generator(t)[0] for t in sim.t]
+    zadanaD = np.zeros_like(sim.t)
+    zadanaD = [generator(t)[1] for t in sim.t]
+    
+    plt.figure()
+    plt.title(title)
+    plt.grid()
+    plt.xlabel("t [s]")
+    plt.ylabel("U [v]")
+    plt.plot(sim.t, sim.y[0], label="$V_o$")
+    #plt.plot(sim.t, sim.y[1], label="$x_2$")
+    plt.plot(sim.t, zadana, "--", label="$V_d$")
+    #plt.plot(sim.t, zadanaD, "--", label="$x_{2D}$")
+    plt.legend()
+
+wc = 1
+k1 = (c*l*wc**2 - 1) / Vin
+k2 = (c*l/Vin) * (2*wc - 1/(c*r))
+K = np.array([k1, k2])
+res = solve_ivp(deg1, [0,10], [2,0], rtol=1e-10, atol=1e-10)
+
+makeGraph("Zadanie śledzenia trajektorii (u_D zamiast u_FF) dla $\omega = 1$", res)
+
+wc = 5
+k1 = (c*l*wc**2 - 1) / Vin
+k2 = (c*l/Vin) * (2*wc - 1/(c*r))
+K = np.array([k1, k2])
+res = solve_ivp(deg1, [0,10], [2,0], rtol=1e-10, atol=1e-10)
+
+makeGraph("Zadanie śledzenia trajektorii (u_D zamiast u_FF) dla $\omega = 5$", res)
+
+wc = 10
+k1 = (c*l*wc**2 - 1) / Vin
+k2 = (c*l/Vin) * (2*wc - 1/(c*r))
+K = np.array([k1, k2])
+res = solve_ivp(deg1, [0,10], [2,0], rtol=1e-10, atol=1e-10)
+
+makeGraph("Zadanie śledzenia trajektorii (u_D zamiast u_FF) dla $\omega = 10$", res)
+
+plt.show()
--- lab5/lab5_2_3.py
+++ lab5/lab5_2_3.py
@@ -11,13 +11,21 @@ A = np.array([[0,1], [-1/(c*l), -1/(c*r)]])
 B = np.array([[0], [Vin/(c*l)]])
 C = np.array([[1, 0]])
 
+w = 5
+l1 = 2*w - 1/(c*r)
+l2 = w**2 - 2*w/(c*r) + 1/(c**2*r**2) - 1/(c*l)
+L = np.array([[l1], [l2]])
+
 def generator(t):
     vd = 3 + 2*np.sin(t)
     vdD = 2*np.cos(t)
     vdDD = -2*np.sin(t)
     return [vd, vdD, vdDD]
 
-def deg1(t, x):
+def deg1(t, X):
+    x = X[0:2]
+    xe = X[2:4]
+
     [vd, vdD, vdDD] = generator(t)
     xd = np.array([vd, vdD]).T
 
@@ -27,13 +35,17 @@ def deg1(t, x):
     u = ufb + uff
 
     dx = A @ x + B[:, 0] * u
-    return dx
+
+    y = C@x
+    dxe = A @ xe + B[:, 0] * u + L @ (y - C @ xe)
+
+    return np.concatenate((dx, dxe))
 
 wc = 1
 k1 = (c*l*wc**2 - 1) / Vin
 k2 = (c*l/Vin) * (2*wc - 1/(c*r))
 K = np.array([k1, k2])
-res = solve_ivp(deg1, [0,10], [2,0], rtol=1e-10, atol=1e-10)
+res = solve_ivp(deg1, [0,10], [2,0,0,0], rtol=1e-10, atol=1e-10)
 
 zadana = np.zeros_like(res.t)
 zadana = [generator(t)[0] for t in res.t]
@@ -41,11 +53,18 @@ zadanaD = np.zeros_like(res.t)
 zadanaD = [generator(t)[1] for t in res.t]
 
 plt.figure()
-plt.title("Stan układu")
+plt.title("Estymacja stanu przy zadaniu śledzenia trajektori - $u_{FB}=K(x_d-x)$")
+plt.xlabel("t [s]")
+plt.ylabel("U [v]")
 plt.grid()
 plt.plot(res.t, res.y[0], label="$x_1$")
-plt.plot(res.t, res.y[1], label="$x_2$")
+#plt.plot(res.t, res.y[1], label="$x_2$")
+
+plt.plot(res.t, res.y[2], "--", label="$\hat{x_1}$")
+#plt.plot(res.t, res.y[3], label="$\hat{x_2}$")
+
 plt.plot(res.t, zadana, "--", label="$x_{1D}$")
-plt.plot(res.t, zadanaD, "--", label="$x_{2D}$")
+#plt.plot(res.t, zadanaD, "--", label="$x_{2D}$")
+
 plt.legend()
 plt.show()
